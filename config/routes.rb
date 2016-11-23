72237ad68753dc83725a0a17a71aea4c8477a961
Yohei Senju <yohei.senju@gmail.com>
2016-11-23 21:49:26 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
+  resources :users do
+    collection do
+      get :skills
+      get :people
+    end
+    member do
+      get :add_skill
+      post :approve_skill
+    end
+  end
+  get    'signup'  => 'users#new'
+  get    'login'   => 'session#new'
+  post   'login'   => 'session#create'
+  delete 'logout'  => 'session#destroy'
 end
